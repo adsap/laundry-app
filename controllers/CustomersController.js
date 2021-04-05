de4a7cbabfdfa0dba39b0321d158cf714cb494c4
okihusni <okihusni@gmail.com>
2021-04-05 23:40:00 +0700

    customer-crud-basic

--- controllers/CustomersController.js
+++ controllers/CustomersController.js
@@ -1,5 +1,95 @@
-class CustomersController {
+const { Customer } = require('../models')
 
+class CustomersController {
+  static list(req, res) {
+    Customer.findAll()
+    .then(customers => {
+      res.render('customers', { customers })
+    })
+    .catch(err => {
+      res.send(err)
+    })
+  }
+
+  static addForm(req, res) {
+    res.render('customerAddForm')
+  }
+
+  static add(req, res) {
+    const { name, phone, email, address } = req.body
+
+    Customer.create({ name, phone, email, address })
+    .then(customer => {
+      res.redirect('/customer')
+    })
+    .catch(err => {
+      res.send(err)
+    })
+  }
+
+  static editForm(req, res) {
+    const { id } = req.params
+
+    Customer.findByPk(+id)
+    .then(customer => {
+      res.render('customerEditForm', { customer })
+    })
+    .catch(err => {
+      res.send(err)
+    })
+  }
+
+  static edit(req, res) {
+    const { id } = req.params
+    const { name, phone, email, address } = req.body
+
+    Customer.findByPk(+id)
+    .then(customer => {
+      if(customer) {
+        customer.name = name
+        customer.phone = phone
+        customer.email = email
+        customer.address = address
+
+        return customer.save()
+      }
+      else throw 'customer tidak ditemukan'
+    })
+    .then(customer => {
+      res.redirect('/customer')
+    })
+    .catch(err => {
+      res.send(err)
+    })
+  }
+
+  static delete(req, res) {
+    const { id } = req.params
+
+    Customer.findByPk(+id)
+    .then(customer => {
+      if(customer) return customer.destroy()
+      else throw 'customer tidak ditemukan'
+    })
+    .then(() => {
+      res.redirect('/customer')
+    })
+    .catch(err => {
+      res.send(err)
+    })
+  }
+
+  static detail(req, res) {
+    const { id } = req.params
+
+    Customer.findByPk(+id)
+      .then(costumer => {
+        res.render('customerDetail')
+      })
+      .catch(err => {
+        res.send(err)
+      })
+  }
 }
 
 module.exports = CustomersController
--- routes/customer.js
+++ routes/customer.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
+const CustomersController = require('../controllers/CustomersController')
 
-router.get('/', CustomerController.list)
-router.get('/add', CustomerController.addFprm)
-router.post('/add', CustomerController.add)
-router.get('/edit/:id', CustomerController.editForm)
-router.post('/edit/:id', CustomerController.edit)
-router.get('/delete/:id', CustomerController.delete)
-router.get('/:id', CustomerController.detail)
+router.get('/', CustomersController.list)
+router.get('/add', CustomersController.addForm)
+router.post('/add', CustomersController.add)
+router.get('/edit/:id', CustomersController.editForm)
+router.post('/edit/:id', CustomersController.edit)
+router.get('/delete/:id', CustomersController.delete)
+router.get('/:id', CustomersController.detail)
 
 module.exports = router
