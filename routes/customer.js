75da4f52306285e084ba49bc5a2d277c8e0f0c88
okihusni <okihusni@gmail.com>
2021-04-05 21:32:05 +0700

    commit-2

--- routes/customer.js
+++ routes/customer.js
@@ -1,3 +1,11 @@
 const router = require('express').Router()
 
+router.get('/', CustomerController.list)
+router.get('/add', CustomerController.addFprm)
+router.post('/add', CustomerController.add)
+router.get('/edit/:id', CustomerController.editForm)
+router.post('/edit/:id', CustomerController.edit)
+router.get('/delete/:id', CustomerController.delete)
+router.get('/:id', CustomerController.detail)
+
 module.exports = router
